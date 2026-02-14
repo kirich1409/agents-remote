d3a6464cac599111434b34a1a239cdd73d322e68
kirich1409 <kirill@androidbroadcast.dev>
2026-02-14 19:35:45 +0300

    chore: initialize gradle multiplatform project structure

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("multiplatform") version "2.3.20-Beta2" apply false
+    kotlin("plugin.serialization") version "2.3.20-Beta2" apply false
+    id("org.jetbrains.compose") version "1.10.1" apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("app.cash.sqldelight") version "2.0.2" apply false
+}
+
+allprojects {
+    // Global Kotlin compiler options for strict code quality
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+            allWarningsAsErrors.set(true)
+            freeCompilerArgs.addAll(
+                "-Xexplicit-api=strict",  // Require explicit API declarations
+                "-opt-in=kotlin.RequiresOptIn",
+                "-opt-in=kotlin.ExperimentalStdlibApi",
+                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
+            )
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://plugins.gradle.org/m2/")
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+rootProject.name = "remote-cloud-code"
+
+include(":shared")
+include(":backend")
+include(":app")
