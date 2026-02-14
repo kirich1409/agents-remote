5b87d5836555bf5a413feeb304b90e16e1e87e78
kirich1409 <kirill@androidbroadcast.dev>
2026-02-14 19:39:59 +0300

    fix: update KSP version to match Kotlin 2.3.20-Beta2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.jetbrains.compose") version "1.10.1" apply false
     id("com.android.application") version "8.2.0" apply false
     id("com.android.library") version "8.2.0" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.google.devtools.ksp") version "2.3.20-Beta2-1.0.17" apply false
     id("app.cash.sqldelight") version "2.0.2" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.6" apply false
     id("org.jmailen.gradle.kotlinter") version "4.1.1" apply false
